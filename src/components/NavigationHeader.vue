b58c581262320bde184a39d929179b8fd1f75a75
Brayan Perez <brayanperezbuitrago@gmail.com>
2024-02-07 23:22:14 -0500

    Added Css code to navBar

--- src/components/NavigationHeader.vue
+++ src/components/NavigationHeader.vue
@@ -1,17 +1,17 @@
 <template>
   <nav class="nav-header">
     <div class="nav-container">
-      <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
+      <a href="https://flowbite.com/" class=" nav-logo-container">
         <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo">
-        <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+        <span class="nav-logo-text">Ewas</span>
       </a>
-      <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+      <div class="nav-button-container">
         <button type="button"
-          class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+          class="nav-button1">
           Get started
         </button>
         <button data-collapse-toggle="navbar-sticky" type="button"
-          class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          class="nav-button-menu"
           aria-controls="navbar-sticky" aria-expanded="false">
           <span class="sr-only">Open main menu</span>
           <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
@@ -20,28 +20,19 @@
           </svg>
         </button>
       </div>
-      <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
-        <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50
-                   md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800
-                   md:dark:bg-gray-900 dark:border-gray-700">
+      <div class="nav-container-menu" id="navbar-sticky">
+        <ul class="nav-menu-list">
           <li>
-            <a href="#" class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 
-                       md:p-0 md:dark:text-blue-500" aria-current="page">Home</a>
+            <a href="#" class="nav-menu-item-selected" aria-current="page">Home</a>
           </li>
           <li>
-            <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent 
-                       md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700
-                       dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
+            <a href="#" class="nav-menu-item">About</a>
           </li>
           <li>
-            <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent 
-                       md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700
-                       dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</a>
+            <a href="#" class="nav-menu-item">Services</a>
           </li>
           <li>
-            <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent 
-                       md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700
-                       dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
+            <a href="#" class="nav-menu-item">Contact</a>
           </li>
         </ul>
       </div>
@@ -59,26 +50,147 @@ export default defineComponent({
 });
 </script>
 <style lang="scss" scoped>
-.nav-header {
- @apply bg-white 
-  min-h-28
-  dark:bg-gray-900 
-  fixed 
-  w-full 
-  z-20 
-  top-0 
-  start-0 
-  border-b 
-  border-gray-200 
-  dark:border-gray-600;
-}
-.nav-container {
-  @apply max-w-screen-xl 
-  flex 
-  flex-wrap 
-  items-center 
-  justify-between 
-  mx-auto 
-  p-4;
-}
+  .nav-header {
+   @apply 
+      bg-white 
+      dark:bg-gray-900 
+      fixed 
+      w-full 
+      z-20 
+      top-0 
+      start-0 
+      border-b 
+      border-gray-200 
+      dark:border-gray-600;
+  }
+  .nav-container {
+    @apply 
+      max-w-screen-xl 
+      flex 
+      flex-wrap 
+      items-center 
+      justify-between 
+      mx-auto 
+      p-4;
+  }
+  .nav-logo-container{
+    @apply 
+      flex 
+      items-center 
+      space-x-3 
+      rtl:space-x-reverse;
+  }
+  .nav-logo-text{
+    @apply 
+      self-center 
+      text-2xl 
+      font-semibold 
+      whitespace-nowrap 
+      dark:text-white;
+  }
+  .nav-button-container{
+    @apply 
+      flex 
+      md:order-2 
+      space-x-3 
+      md:space-x-0 
+      rtl:space-x-reverse;
+  }
+  .nav-button-menu {
+    @apply 
+      inline-flex 
+      items-center 
+      p-2 
+      w-10 
+      h-10 
+      justify-center 
+      text-sm 
+      text-gray-500 
+      rounded-lg 
+      md:hidden 
+      hover:bg-gray-100 
+      focus:outline-none 
+      focus:ring-2 
+      focus:ring-gray-200 
+      dark:text-gray-400 
+      dark:hover:bg-gray-700 
+      dark:focus:ring-gray-600;
+  }
+  .nav-button1 {
+    @apply 
+      text-white 
+      bg-blue-700 
+      hover:bg-blue-800 
+      focus:ring-4 
+      focus:outline-none 
+      focus:ring-blue-300 
+      font-medium 
+      rounded-lg 
+      text-sm 
+      px-4 
+      py-2 
+      text-center 
+      dark:bg-blue-600 
+      dark:hover:bg-blue-700 
+      dark:focus:ring-blue-800;
+  }
+  .nav-menu-container {
+    @apply items-center 
+      justify-between 
+      hidden 
+      w-full 
+      md:flex 
+      md:w-auto 
+      md:order-1;
+  }
+  .nav-menu-list {
+    @apply flex 
+      flex-col 
+      p-4 
+      md:p-0 
+      mt-4 
+      font-medium 
+      border 
+      border-gray-100 
+      rounded-lg 
+      bg-gray-50 
+      md:space-x-8 
+      rtl:space-x-reverse 
+      md:flex-row 
+      md:mt-0 
+      md:border-0 
+      md:bg-white 
+      dark:bg-gray-800 
+      md:dark:bg-gray-900 
+      dark:border-gray-700;
+  }
+  .nav-menu-item{
+    @apply block 
+      py-2 
+      px-3 
+      text-gray-900 
+      rounded 
+      hover:bg-gray-100 
+      md:hover:bg-transparent 
+      md:hover:text-blue-700 
+      md:p-0 
+      md:dark:hover:text-blue-500 
+      dark:text-white 
+      dark:hover:bg-gray-700 
+      dark:hover:text-white 
+      md:dark:hover:bg-transparent 
+      dark:border-gray-700;
+  } 
+  .nav-menu-item-selected{
+    @apply block 
+      py-2 
+      px-3 
+      text-white 
+      bg-blue-700 
+      rounded 
+      md:bg-transparent 
+      md:text-blue-700 
+      md:p-0 
+      md:dark:text-blue-500;
+  }
 </style>
